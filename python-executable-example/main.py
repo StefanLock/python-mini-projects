49b37a403d49eb623c2328b8045873d9ba2e0d4e
StefanLock <stefanthomaslock@gmail.com>
2026-07-27 19:02:11 +0100

    adding some more functionality just for reference

--- python-executable-example/main.py
+++ python-executable-example/main.py
@@ -2,13 +2,24 @@ import click
 import psutil
 
 @click.command()
-def disk_usage():
+@click.option('--threshold', type=int, default=80, help='Threshold for disk usage percentage')
+def disk_usage(threshold):
 
-    click.secho("-- Disk Check --", fg="green", bold=True)
+    click.secho("-- Disk Check --", fg="yellow", bold=True)
 
+    # Disk space information
     disk_use = psutil.disk_usage('/')
     hr = round(disk_use.free / (1024 ** 3), 2)
-    click.echo(f"{hr}GB free disk space, \n{disk_use.percent}% used of total")
+    # Display free disk space, added confirm just for reference
+    if click.confirm(f"Do you want to see the free disk space?"):
+        click.secho(f"Free disk space: {hr} GB", fg="blue", bold=True)
+
+    # Check if disk usage exceeds the threshold
+    if click.confirm(f"Do you want to see the disk usage information?"):
+        if disk_use.percent > threshold:
+            click.secho(f"Warning: Disk usage is above {threshold}%!", fg="red", bold=True)
+        else:
+            click.secho(f"{disk_use.percent}% used of total", fg="green", bold=True)
 
 def main():
     disk_usage()
